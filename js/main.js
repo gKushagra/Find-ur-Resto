0b19257a2e8763eb2fc5fb95418cc91301ac4d2a
gKushagra <55290773+gKushagra@users.noreply.github.com>
2020-03-03 12:04:29 -0600

    Maps API fix
    sensor=false in query string

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var js_file = document.createElement('script');
     js_file.type = 'text/javascript';
-    js_file.src = `https://maps.googleapis.com/maps/api/js?callback=initMap&key=${googleMapsKey}&language=` + lang;
+    js_file.src = `https://maps.googleapis.com/maps/api/js?callback=initMap&key=${googleMapsKey}&sensor=false&language=` + lang;
     document.getElementsByTagName('head')[0].appendChild(js_file);
   }
 });
@@ -104,7 +104,7 @@ function showPosition(position) {
   });
 
   //Fetching and displaying User's Address
-  fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=${googleMapsKey}`)
+  fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=${googleMapsKey}&sensor=false`)
     .then(response => response.json())
     .then(data => {
       document.getElementById("loc").value = data.results[0].formatted_address;
